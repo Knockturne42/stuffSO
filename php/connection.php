f3452603e42a3112841f62ac6cc5ac8411c7f5af
Knockturne42 <dcl.thibauds@18pixel.fr>
2019-02-04 01:14:09 +0100

    add sql table

--- php/connection.php
+++ php/connection.php
@@ -1,5 +1,16 @@
 <?php
+$servername = "localhost";
+$username = "test";
+$password = "test00";
 
-
-
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=testso", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
 ?>
